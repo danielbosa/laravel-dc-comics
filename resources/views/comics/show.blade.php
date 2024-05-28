8c34026b07b271a9f76457f2e2a5a1620c9380e0
danielbosa <danielbosa95@gmail.com>
2024-05-28 07:53:26 +0200

    Created show for comics

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('title', $comic->title)
+@section('content')
+<section class="container bg-white mt-4">
+    <h1 class="text-center">{{$comic->title}}</h1>
+    <div class="row">
+        <div class="col-12 col-md-4">
+            <img src="{{$comic->thumb}}" class="img-fluid" alt="{{$comic->title}}">
+        </div>
+        <div class="col-12 col-md-8">
+            <p>{!!$comic->description!!}</p>
+            <div>
+                Series: {{$comic->series}}
+            </div>
+            <div>
+                Type: {{$comic->type}}
+            </div>
+            <div>
+                Price: {{$comic->price}}
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
