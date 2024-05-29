caef1c4b85b568dc590d8faa6446c669e730e6dc
danielbosa <danielbosa95@gmail.com>
2024-05-29 22:20:53 +0200

    Fixed edit and delete functions in show. Styled.

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 @section('title', $comic->title)
 @section('content')
-<section class="container bg-white mt-4">
+<section class="container bg-white pt-4" id="show">
     <h1 class="text-center">{{$comic->title}}</h1>
-    <div class="row">
+    <div class="row pb-3">
         <div class="col-12 col-md-4">
             <img src="{{$comic->thumb}}" class="img-fluid" alt="{{$comic->title}}">
         </div>
@@ -18,8 +18,33 @@
             <div>
                 Price: {{$comic->price}}
             </div>
-            {{-- Bottoni per modificare e cancellare --}}
+            <div class="py-3 d-flex gap-2">
+                <a href="{{route('comics.edit', $comic->id)}}" class="db-btn db-btn-1">Edit</a>
+                <form action="{{route('comics.destroy', $comic->id)}}" method="POST" class="db-btn db-btn-1">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Delete" class="BtnToConfirm">
+                </form>
+            </div>
         </div>
     </div>
 </section>
+{{-- modal --}}
+<div class="modal fade" id="indexModal" tabindex="-1" aria-labelledby="indexModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="indexModalLabel">ATTENTION: this action cannot be undone.</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            Would you like to continue?
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+            <button type="button" class="btn btn-primary">Yes</button>
+        </div>
+        </div>
+    </div>
+</div> 
 @endsection
